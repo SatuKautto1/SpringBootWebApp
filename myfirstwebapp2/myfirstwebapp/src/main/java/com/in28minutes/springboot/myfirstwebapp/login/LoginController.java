1f2230123907d600f272fa43265137861018211c
SatuKautto1 <satu.kautto@edu.karelia.fi>
2022-10-11 13:39:10 +0300

    testi
    testi

---
+++ myfirstwebapp2/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,47 @@
+package com.in28minutes.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+
+@Controller
+@SessionAttributes ("name")
+public class LoginController {
+	
+	private AuthenticationService authenticationService;
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+	//loging
+	// GET, POST
+	
+	//avaa login sivun, huom GET
+	@RequestMapping(value="login", method = RequestMethod.GET)
+	public String gotoLogingPage() {
+		return "login";
+	}
+	
+	//nimen ja salasanan jälkeen avaa welcomesivun, huom POST (salaista tietoa)
+	@RequestMapping(value="login", method = RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+		
+		if(authenticationService.authenticate(name, password)) {
+		model.put("name",name);
+		//model.put("password",password);
+		
+		//Authentication
+		//valid name = in28minutes
+		// valid password = dummy
+		return "welcome";
+		}
+		model.put("errorMessage", "Invalid Credentials! Please try again");
+		return "login";
+	}	
+	
+}
