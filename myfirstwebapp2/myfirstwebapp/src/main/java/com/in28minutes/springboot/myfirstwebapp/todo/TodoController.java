3d5d0262334ad0607e49238a11d161da7d65d8cb
SatuKautto1 <satu.kautto@edu.karelia.fi>
2022-10-11 15:46:56 +0300

    tiistai
    tiistain tekeleet

--- myfirstwebapp2/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp2/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -23,8 +26,15 @@ public class TodoController {
 		return "listTodos";
 	}
 	
-	@RequestMapping("add-todo")
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
 	public String showNewTodoPage() {
-		return "Todo";
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		String username = (String)model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
 	}
 }
