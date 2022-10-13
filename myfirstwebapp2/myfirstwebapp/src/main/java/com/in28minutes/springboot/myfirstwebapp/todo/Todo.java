2d06aceaaf22ff5ddd8f14cd794f4520a0110bfe
SatuKautto1 <satu.kautto@edu.karelia.fi>
2022-10-13 10:41:20 +0300

    talteen
    talteen

--- myfirstwebapp2/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp2/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,13 +2,11 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
+
 public class Todo {
 
-	private int id;
-	private String username;
-	private String description;
-	private LocalDate targetDate;
-	private boolean done;
+	
 	
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
@@ -18,6 +16,15 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.done = done;
 	}
+	
+	private int id;
+	private String username;
+	
+	@Size(min=10, message="Enter atleast 10 characters")
+	private String description;
+	private LocalDate targetDate;
+	private boolean done;
+	
 
 	public int getId() {
 		return id;
